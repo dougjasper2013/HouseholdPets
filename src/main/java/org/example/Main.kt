0131f8d0173bd6862c72ab782ed984b7a07def70
Douglas Jasper <doug.jasper@trios.com>
2025-11-17 08:57:51 -0500

    Created Dog and Objects

--- src/main/java/org/example/Main.kt
+++ src/main/java/org/example/Main.kt
@@ -2,4 +2,18 @@ package org.example
 
 fun main() {
     println("Welcome to the HouseholdPets Application")
+
+    val myCat = Cat("Garfield", 8)
+
+    myCat.info()
+    myCat.makeSound()
+    myCat.scratch()
+
+    println()
+
+    val myDog = Dog("Odie", 5)
+
+    myDog.info()
+    myDog.makeSound()
+    myDog.fetch()
 }
